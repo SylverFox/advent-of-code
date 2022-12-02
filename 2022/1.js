912bd219abc88ffb289f13b86cc7d45cf699912e
Joris Diesvelt <joris@elnino.tech>
2022-12-02 16:42:22 +0100

    2022/1

---
+++ 2022/1.js
@@ -0,0 +1,17 @@
+export default async function(input) {
+    let buckets = [], index = 0
+    for (let i of input) {
+        if (i === '') {
+            index++
+        } else if (!buckets[index]) {
+            buckets[index] = [i/1]
+        } else {
+            buckets[index].push(i/1)
+        }
+    }
+
+    buckets = buckets.map(b => b.reduce((a,v) => a + v)).sort((a,b) => b - a)
+
+    console.log('Part 1:', buckets[0])
+    console.log('Part 2:', buckets.slice(0, 3).reduce((a,b) => a+b))
+}

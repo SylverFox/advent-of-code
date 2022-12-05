a1f693d2f504529c7e60fc8942e9ae23584f3210
Joris Diesvelt <joris@elnino.tech>
2022-12-05 11:32:44 +0100

    Add 2022/5

--- common.js
+++ common.js
@@ -1,3 +1,7 @@
-Array.prototype.tap = function(f) {
-    f(this); return this;
-}
+Object.defineProperty(Array.prototype, "tap", {
+    enumerable: false,
+    value: function(f) {
+        f(this)
+        return this
+    }
+});

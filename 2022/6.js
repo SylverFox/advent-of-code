437fff7e5cd2766d925212f0ce24fdbb768f899e
Joris Diesvelt <joris@elnino.tech>
2022-12-06 18:33:14 +0100

    Add 2022 6

---
+++ 2022/6.js
@@ -0,0 +1,18 @@
+export default function (input) {
+    input = input[0].split('')
+    let output
+    for (let i = 0; i < input.length - 3; i++) {
+        if (input.slice(i, i + 4).filter((e,i,a) => a.indexOf(e) === i).length === 4) {
+            output = i + 4
+            break
+        }
+    }
+    console.log('Part 1:', output)
+
+    for (let i = 0; i < input.length - 13; i++) {
+        if (input.slice(i, i + 14).filter((e,i,a) => a.indexOf(e) === i).length === 14) {
+            output = i + 14
+            break
+        }
+    }
+}

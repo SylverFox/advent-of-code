cc3bd51454abdbe9a4d077988a163cf750538e2d
Joris Diesvelt <jorisdiesvelt@gmail.com>
2023-07-30 21:20:45 +0200

    Fix imports on multi os

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import moment from 'moment'
 import fs from 'fs'
+import os from 'os'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import './common.js'
@@ -16,7 +17,7 @@ function parseInput() {
         year = Number(process.argv[2])
     } else {
         year = moment().year()
-        const files = fs.readdirSync(`${DIRNAME}/${year}`)
+        const files = fs.readdirSync(path.resolve(DIRNAME, year.toString()))
             .filter(f => f.endsWith('.js'))
             .map(f => f.slice(0, -3))
             .sort((a, b) => a - b)
@@ -26,15 +27,16 @@ function parseInput() {
 }
 
 function getInputFile(year, puzzle) {
-    const file = `${DIRNAME}/${year}/${puzzle}.txt`
-    return fs.existsSync(file) ? fs.readFileSync(file).toString().split('\n') : ''
+    const file = path.resolve(DIRNAME, year.toString(), puzzle + '.txt')
+    return fs.existsSync(file) ? fs.readFileSync(file).toString().split('\r\n') : ''
 }
 
 const [year, puzzle] = parseInput()
 const input = getInputFile(year, puzzle)
+const solver = path.resolve(DIRNAME, year + '', puzzle + '.js')
 
 console.log(`Running puzzle ${year}/${puzzle}`)
 const time = Date.now()
-import(`${DIRNAME}/${year}/${puzzle}.js`)
+import(os.platform() === 'win32' ? 'file://' + solver : solver)
     .then(puzzle => puzzle.default(input))
     .then(() => console.log('Solution took', Date.now() - time, 'ms'))

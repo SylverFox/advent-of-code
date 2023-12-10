a928ad6d918ef367eb10c0a64a6058a81ecf62fc
Joris Diesvelt <joris@elnino.tech>
2023-12-10 14:49:35 +0100

    Solve 2023/9

---
+++ 2023/9.js
@@ -0,0 +1,10 @@
+export default function (input) {
+    input = input.map(i => i.split(' ').map(Number))
+
+    const diff = arr => arr.reduce((o,v,i,a) => i === 0 ? o : [...o, v-a[i-1]], [])
+    const getNext = arr => arr.every(n => n === 0) ? 0 : getNext(diff(arr)) + arr[arr.length - 1]
+    const getPrevious = arr => arr.every(n => n === 0) ? 0 : arr[0] - getPrevious(diff(arr))
+
+    console.log('part 1:', input.map(getNext).sum())
+    console.log('part 2:', input.map(getPrevious).sum())
+}
--- common.js
+++ common.js
@@ -13,6 +13,19 @@ Object.defineProperty(Array.prototype, "clone", {
     }
 })
 
+Object.defineProperty(Array.prototype, "sum", {
+    enumerable: false,
+    value: function(f) {
+        return this.reduce((a,v) => a + v, 0)
+    }
+})
+
+Object.defineProperty(Array.prototype, "product", {
+    enumerable: false,
+    value: function(f) {
+        return this.reduce((a,v) => a * v, 0)
+    }
+})
 
 export const gcd = (a, b) => !b ? a : gcd(b, a % b)
 export const lcm = (a, b) => (a * b) / gcd(a, b)

7b466c0ee400457b2fdc635d634ef105f476857b
Joris Diesvelt <j61695@eon.com>
2024-12-04 11:08:08 +0100

    solve 2016/7

--- 2016/7.js
+++ 2016/7.js
@@ -5,14 +5,14 @@ export default function (input) {
       i.match(/\[\w*(\w)(?!\1)(\w)\2\1\w*\]/),
     ])
     .filter((i) => i[0] && !i[1])
-    .tap((arr) => console.log('Part 1:', arr.length))
+    .tap((arr) => console.log('part 1:', arr.length))
 
   input
-    .map((i) => [
-      i.match(/(\w)(?!\1)(\w)\1.*\[\w*(\2\1\2)\w*\]/),
-      i.match(/\[\w*(\w)(?!\1)(\w)\1\w*\].*(\2\1\2)/),
-    ])
-    .filter((i) => i[0] || i[1])
-    .tap((arr) => console.log('Part 2:', arr.length))
-  // < 289
+    .map((i) =>
+      Array.from(i.matchAll(/(?<=\[\w*?)((\w)(?!\2)(?=(\w)\2))/g))
+        .map((match) => match[3] + match[2] + match[3])
+        .filter((match) => i.match(new RegExp(`(^|])\\w*${match}`)))
+    )
+    .filter((i) => i.length)
+    .tap((arr) => console.log('part 2:', arr.length))
 }

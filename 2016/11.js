358824afe0ea8264ec14ca0793fa919fc037021e
Joris Diesvelt <j61695@eon.com>
2024-12-04 18:59:06 +0100

    Solve 2016/11

--- 2016/11.js
+++ 2016/11.js
@@ -1,84 +1,142 @@
 export default function (input) {
-  input = input
-    .map((i) => i.match(/\S+ (?:microchip|generator)/g) || [])
-    .map((i) =>
-      i.map((x) =>
-        x.replace(
-          /^([\w-]+)\s(\w+)$/,
-          (_, e, t) => e[0].toUpperCase() + e[1] + t[0].toUpperCase()
-        )
-      )
-    )
+  const MAX_FLOOR = 3
+  // Prune branches that have this score distance
+  const PRUNE_DIST = 4
 
-  let states = [{ map: input.clone(), elevator: 0 }],
-    steps = 0,
-    cache = []
-  main: while (true) {
-    let newStates = []
-    layer: while (states.length) {
-      const next = states.shift()
+  const cloneState = (state) => JSON.parse(JSON.stringify(state))
+  const hashState = (state) =>
+    [
+      state.elevator,
+      ...state.floors.map((f) =>
+        f
+          .map((x) => x.name[0] + x.name[1] + x.type[0])
+          .sort((a, b) => a.localeCompare(b))
+      ),
+    ].join(';')
+  const scoreState = (state) =>
+    state.floors.map((f, i) => f.length * (i + 1)).sum()
 
-      // Success state
-      if (next.map.slice(0, -1).every((floor) => !floor.length)) {
-        // Everything is on the last floor
-        break main
-      }
+  // if a floor contains at least one generator, then every microchip needs an accompanying generator
+  const checkFloor = (floor) =>
+    floor.some((f) => f.type === 'generator')
+      ? floor
+          .filter((f) => f.type === 'microchip')
+          .every((m) =>
+            floor.some((g) => g.type === 'generator' && g.name === m.name)
+          )
+      : true
 
-      // Expand state
-      const dirs = []
-      if (next.elevator > 0) dirs.push(next.elevator - 1)
-      if (next.elevator < next.map.length - 1) dirs.push(next.elevator + 1)
+  const calculateSteps = (initialState) => {
+    let states = [initialState],
+      steps = 0,
+      cache = new Set(),
+      minScore = scoreState(initialState)
+    main: while (states.length) {
+      let newStates = []
+      let newMinScore = minScore
 
-      let permutations = []
-      for (let dir of dirs) {
-        for (let i = 0; i < next.map[next.elevator].length; i++) {
-          permutations.push([dir, i])
-          for (let j = i + 1; j < next.map[next.elevator].length; j++) {
-            permutations.push([dir, i, j])
-          }
+      layer: while (states.length) {
+        const next = states.shift()
+
+        // Duplicate state
+        const hash = hashState(next)
+        next.log.push(hash)
+        if (cache.has(hash)) {
+          continue
+        } else {
+          cache.add(hash)
         }
-      }
-      // console.log('expand', next.map)
-      perms: for (let p of permutations) {
-        const mapCopy = next.map.clone()
-        for (let i = p.length - 1; i > 0; i--) {
-          mapCopy[p[0]].push(mapCopy[next.elevator].splice(p[i], 1)[0])
+
+        // Score state
+        const score = scoreState(next)
+        if (score < minScore - PRUNE_DIST) {
+          continue
+        } else {
+          newMinScore = Math.max(newMinScore, score)
         }
-        // console.log(mapCopy)
 
-        const newState = { map: mapCopy, elevator: p[0] }
-        if (cache.includes(JSON.stringify(newState))) continue perms
+        // Success state
+        if (next.floors.slice(0, -1).every((floor) => !floor.length)) {
+          return steps
+        }
 
-        // Fail state
-        for (let f = 0; f < mapCopy.length; f++) {
-          for (let item of mapCopy[f]) {
+        // Expand state
+        let directions = []
+        if (next.elevator > 0) directions.push(-1)
+        if (next.elevator < MAX_FLOOR) directions.push(1)
+
+        const availableItems = next.floors[next.elevator]
+        let itemSet = []
+        // move single items
+        itemSet.push(...availableItems.map((i) => [i]))
+        // move two items
+        for (let i = 0; i < availableItems.length - 1; i++) {
+          for (let j = i + 1; j < availableItems.length; j++) {
+            // A combination of a generator and microchip of different types is never possible
             if (
-              item.endsWith('M') &&
-              !mapCopy[f].includes(item.slice(0, 2) + 'G') &&
-              mapCopy[f].find((x) => x.endsWith('G'))
+              availableItems[i].type !== availableItems[j].type &&
+              availableItems[i].name !== availableItems[j].name
             ) {
-              // console.log('invalid', next.map, f, item)
-              continue perms
+              continue
             }
+
+            itemSet.push([availableItems[i], availableItems[j]])
           }
         }
 
-        newStates.push({ map: mapCopy, elevator: p[0] })
-        cache.push(JSON.stringify(next))
+        for (let dir of directions) {
+          for (let set of itemSet) {
+            const resultCurFloor = next.floors[next.elevator].filter((f) =>
+              set.every((s) => s.name !== f.name || s.type !== f.type)
+            )
+            const resultNextFloor = [
+              ...next.floors[next.elevator + dir],
+              ...set,
+            ]
+
+            if (!checkFloor(resultCurFloor) || !checkFloor(resultNextFloor)) {
+              // invalid states for some floor
+              continue
+            }
+
+            const newState = cloneState(next)
+            // remove set items from floor
+            newState.floors[newState.elevator] = resultCurFloor
+            // add set items to new floor
+            newState.floors[newState.elevator + dir] = resultNextFloor
+            // set new floor
+            newState.elevator += dir
+            newStates.push(newState)
+          }
+        }
       }
-    }
 
-    states = newStates
-    steps++
-
-    console.log(steps, states.length)
-    // states.forEach(s => console.log(s.elevator, s.map))
-    // console.log()
-    // cache.forEach(c => console.log(JSON.parse(c)))
-    // console.log()
-    if (steps === 12) {
-      break
+      states = newStates
+      minScore = newMinScore
+      steps++
+      console.log(steps, states.length)
     }
   }
-  console.log('Part 1:', steps)
+
+  let initialState = {
+    floors: input
+      .map((i) => i.match(/\S+ (microchip|generator)/g) || [])
+      .map((i) => i.map((x) => x.split(/(-compatible)?\s/)))
+      .map((i) => i.map((x) => ({ name: x[0], type: x[2] }))),
+    elevator: 0,
+    log: [],
+  }
+
+  console.log('part 1:', calculateSteps(initialState))
+  // 33
+
+  initialState.floors[0].push(
+    { name: 'elerium', type: 'microchip' },
+    { name: 'elerium', type: 'generator' },
+    { name: 'dilithium', type: 'microchip' },
+    { name: 'dilithium', type: 'generator' }
+  )
+
+  console.log('part 2:', calculateSteps(initialState))
+  // 57
 }

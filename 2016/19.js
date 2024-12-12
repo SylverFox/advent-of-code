4ad2e03854460dcb67af0ae6cf87f597f7c26079
Joris Diesvelt <j61695@eon.com>
2024-12-12 19:59:09 +0100

    Solve 2016/19

--- 2016/19.js
+++ 2016/19.js
@@ -1,3 +1,27 @@
 export default function (input) {
-  //
+  input = Number(input[0])
+
+  let ans = 1,
+    num = input
+  for (let it = 1; num > 1; it++) {
+    if (num % 2 === 0) {
+      num /= 2
+    } else {
+      ans += 2 ** it
+      num = (num - 1) / 2
+    }
+  }
+  console.log('part 1:', ans)
+
+  let l = 1,
+    w = 1
+  for (let i = 0; i < input; i++) {
+    if (w + 1 > i) {
+      l = w
+      w = 1
+    } else {
+      w += w < l ? 1 : 2
+    }
+  }
+  console.log('part 2:', w)
 }
